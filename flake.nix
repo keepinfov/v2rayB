737f7f0d8b8471d18ccf4476591c10ee3239560a
Keepinfov <218463407+keepinfov@users.noreply.github.com>
2026-01-27 16:45:11 +0300

    feat: improve Nix flake, update GUI deps, and add dark theme (#6)
    * chore: update gitignore for Nix and generated files
    
    * feat(nix): improve flake with working vendorHash and desktop build
    
    * chore(gui): update dependencies and add npmrc
    
    * feat(gui): add dark theme SCSS variables
    
    * chore(desktop): add yarn lockfile

--- flake.nix
+++ flake.nix
@@ -10,31 +10,20 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-
         version = "2.0.0";
-
-        webUI = pkgs.buildNpmPackage {
-          pname = "v2rayb-web";
-          inherit version;
-          src = ./gui;
-          npmDepsHash = "sha256-0000000000000000000000000000000000000000000=";
-          buildPhase = ''
-            npm run build
-          '';
-          installPhase = ''
-            cp -r dist $out
-          '';
-        };
 
         v2rayb = pkgs.buildGoModule {
           pname = "v2rayb";
           inherit version;
-          src = ./service;
-          vendorHash = null;
+          src = ./.;
+          vendorHash = "sha256-uiURsB1V4IB77YKLu5gdaqw9Fuja6fC5adWYDE3OE+Q=";
 
-          preBuild = ''
-            mkdir -p server/router/web
-            cp -r ${webUI}/* server/router/web/
+          modRoot = "./service";
+          doCheck = false;
+
+          postUnpack = ''
+            mkdir -p $sourceRoot/service/server/router/web
+            cp -r $sourceRoot/web/* $sourceRoot/service/server/router/web/
           '';
 
           ldflags = [
@@ -48,14 +37,46 @@
           };
         };
 
+        desktopLibs = with pkgs; [
+          webkitgtk_4_1
+          gtk3
+          cairo
+          gdk-pixbuf
+          glib
+          dbus
+          openssl
+          librsvg
+          libsoup_3
+        ];
+
+        v2rayb-desktop = pkgs.rustPlatform.buildRustPackage {
+          pname = "v2rayb-desktop";
+          inherit version;
+          src = ./desktop/src-tauri;
+          cargoLock.lockFile = ./desktop/src-tauri/Cargo.lock;
+
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+          buildInputs = desktopLibs;
+
+          postInstall = ''
+            mv $out/bin/v2rayb-desktop $out/bin/v2rayb-desktop || true
+          '';
+
+          meta = {
+            description = "v2rayB Desktop wrapper";
+            mainProgram = "v2rayb-desktop";
+          };
+        };
+
       in {
         packages = {
           default = v2rayb;
-          inherit v2rayb;
+          inherit v2rayb v2rayb-desktop;
         };
 
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ go nodejs yarn ];
+          buildInputs = with pkgs; [ go nodejs yarn cargo rustc pkg-config ] ++ desktopLibs;
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath desktopLibs;
         };
       }
     ) // {
@@ -68,7 +89,7 @@
 
             address = lib.mkOption {
               type = lib.types.str;
-              default = "0.0.0.0:20170";
+              default = "127.0.0.1:50541";
               description = "Listen address";
             };
 
